0fcf79eb03d09831168da292773febb4a4d4445d
DevVitorNunes <dev.vitornunes@gmail.com>
2026-03-16 13:14:42 +0000

    adiciona `.prettierignore`

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,4 @@ function Home() {
   return <h1>Eu consigo usar o git!</h1>;
 }
 
-function teste() {
-  console.log("teste");
-}
-
-function teste2() {
-  console.log("teste oi");
-}
 export default Home;
