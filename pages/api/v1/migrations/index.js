3fe03c8918c2acc2bd5d32082187a3eb6e4f1c9c
DevVitorNunes <dev.vitornunes@gmail.com>
2026-04-06 11:26:16 +0000

    add /api/v1/migrations

---
+++ pages/api/v1/migrations/index.js
@@ -0,0 +1,35 @@
+import migrationRunner from "node-pg-migrate";
+import { join } from "node:path";
+import database from "infra/database.js";
+
+export default async function migrations(request, response) {
+  const dbClient = await database.getNewClient();
+  const defaultMigrations = {
+    dbClient: dbClient,
+    dryRun: true,
+    dir: join("infra", "migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  };
+  if (request.method === "GET") {
+    const migrations = await migrationRunner(defaultMigrations);
+    await dbClient.end();
+    return response.status(200).json(migrations);
+  }
+
+  if (request.method === "POST") {
+    const migrations = await migrationRunner({
+      ...defaultMigrations,
+      dryRun: false,
+    });
+
+    await dbClient.end();
+
+    if (migrations.length > 0) {
+      return response.status(201).json(migrations);
+    }
+    return response.status(200).json(migrations);
+  }
+  response.status(405);
+}
